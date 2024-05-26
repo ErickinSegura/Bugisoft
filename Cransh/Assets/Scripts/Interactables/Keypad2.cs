70bb1001c3ff32326451cfb527d2dffa89ad2176
Saul0Delgado <A01285188@tec.mx>
2024-05-26 11:35:59 -0600

    Casita2

---
+++ Cransh/Assets/Scripts/Interactables/Keypad2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad2 : Interactable
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject Pixel_box_1;
+    private bool BoxOpen;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    protected override void Interact()
+    {
+        BoxOpen = !BoxOpen;
+    }
+}
