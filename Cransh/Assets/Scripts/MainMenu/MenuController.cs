92df641c70781950d2e47670c027400995b6b3bc
ErickinSegura <A01613821@tec.mx>
2024-05-26 17:30:10 -0600

    menu principal y SFX añadido

--- Cransh/Assets/Scripts/MainMenu/MenuController.cs
+++ Cransh/Assets/Scripts/MainMenu/MenuController.cs
@@ -6,10 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    // Instanciar el controlador de SFX
+    public SFXController sfxController;
+
+    // Start is called before the first frame update   
+    void Start()
+    {
+        // Obtener el controlador de SFX
+        sfxController = SFXController.instance;
+        sfxController.PlaySFX(4);
+    }
+
     public void StartGame()
     {
         // Cargar la escena del juego
         SceneManager.LoadScene("Casa");
+        sfxController.PlaySFX(5);
+
     }
 
     public void QuitGame()
