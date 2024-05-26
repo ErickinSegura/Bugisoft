7a125e9a8d091421d409b0f91528ac6b4884b8ad
Fernando Espidio <fer618@hotmail.com>
2024-05-26 14:23:13 -0600

    gestapo corregido god

--- Cransh/Assets/SFX/SFXController.cs
+++ Cransh/Assets/SFX/SFXController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -60,66 +59,3 @@ public class SFXController : MonoBehaviour
     }
 
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SFXController : MonoBehaviour
-{
-    public static SFXController instance;
-    // Audio Source
-    public AudioSource audioSource;
-
-    // Audio Clips
-    public AudioClip[] audioClips;
-
-    // Play SFX
-    public void PlaySFX(int clipIndex)
-    {
-        Debug.Log("Playing SFX: " + clipIndex);
-        audioSource.clip = audioClips[clipIndex];
-        audioSource.Play();
-
-    }
-
-    public void PLayAndStopSFX(int clipIndex)
-    {
-        Debug.Log("Playing SFX: " + clipIndex);
-        audioSource.clip = audioClips[clipIndex];
-        audioSource.Play();
-        StartCoroutine(StopSFXAfterSeconds(audioClips[clipIndex].length));
-    }
-
-    IEnumerator StopSFXAfterSeconds(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        audioSource.Stop();
-    }
-
-    // Stop SFX
-    public void StopSFX()
-    {
-        audioSource.Stop();
-    }
-
-    // Pause SFX
-    public void PauseSFX()
-    {
-        audioSource.Pause();
-    }
-
-    // Unpause SFX
-    public void UnpauseSFX()
-    {
-        audioSource.UnPause();
-    }
-
-    public void Awake()
-    {
-        instance = this;
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-}
->>>>>>> eb9587cc21b140b7ddc5c9534f366afe8d70462e
